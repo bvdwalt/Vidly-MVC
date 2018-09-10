f8cb4ea0c5516d37d3ce3a2d8056387a0311f087
Bennie van der Walt <bennie.vdwalt@entelect.co.za>
2018-09-10 07:27:14 +0200

    Added movie controller

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek" };
+            //return View(movie);
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue) pageIndex = 1;
+
+            if (string.IsNullOrEmpty(sortBy)) sortBy = "Name";
+
+            return Content($"pageIndex={pageIndex}&sortBy={sortBy}");
+        }
+    }
+}
